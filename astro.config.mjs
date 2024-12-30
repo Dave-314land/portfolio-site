9c1b4d0a57fad4e402d8a5a24e7a09300fa74e14
Dave-314land <dave.l.piland@gmail.com>
2024-12-29 23:58:37 -0500

    added github actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,7 @@ export default defineConfig({
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+    site: 'https://dave-314land.github.io/',
+    base: 'portfolio-site',
 });
